8953a1ca6c70bdffdc3a387a3ec38b46d186b8b5
Carlos Knopel <carlos.knopel@gmail.com>
2025-01-21 15:59:54 -0300

    Bump version 1.0.9

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import "./index.css";
 
 export * from "@/components/ui/button";
+export * from "@/components/ui/dialog";
+export * from "@/components/ui/FileSelector";
+export * from "@/components/ui/FileUpload";
+export * from "@/components/ui/ImageList";
+export * from "@/components/ui/input";
+export * from "@/components/ui/label";
+export * from "@/components/ui/modal";
+export * from "@/components/ui/radio-group";
+export * from "@/components/ui/select";
+export * from "@/components/ui/slider";
+export * from "@/components/ui/switch";
+export * from "@/components/ui/tabs";
+export * from "@/components/ui/textarea";
